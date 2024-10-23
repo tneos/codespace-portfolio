26f653eb06ce7bc870d87ad8a464ecd046369262
Thomas Neos <tneos@hotmail.com>
2024-10-23 11:15:19 +0100

    Code challenge submitted

--- Testing Technologies and Programming/coding-challenges/secretNumber.js
+++ Testing Technologies and Programming/coding-challenges/secretNumber.js
@@ -1,21 +1,28 @@
 // Random Number Game
 
 const secretNumber = () => {
+  // Generate random number
   let secretNum = Math.floor(Math.random() * 10);
+  // Input from user
   let choice = parseInt(prompt("Enter your choice(0-9): "));
 
+  // As long as user's guess is not right keep prompting for input
   while (choice !== secretNum) {
-    choice = parseInt(prompt("Enter your choice(0-9): "));
-    if (choice > 9 || choice < 0 || choice === NaN) {
+    // Check if choice is within limit or invalid
+    if (choice > 9 || choice < 0 || isNaN(choice)) {
       console.log("Please add a number between 0 and 9");
     } else if (choice > secretNum) {
       console.log(`You answered ${choice}. The correct answer is lower.Try again`);
     } else if (choice < secretNum) {
       console.log(`You answered ${choice}. The correct answer is higher.Try again`);
-    } else {
-      console.log("Your answer is correct!");
     }
+
+    // Error message if invalid input
+    choice = parseInt(prompt("Enter your choice(0-9): "));
   }
+
+  // Correct message to user
+  console.log(`You answered ${choice}. This is the correct answer!`);
 };
 
 secretNumber();
